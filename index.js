cc5c5bc1b20869bfe12aa64941e342e458489301
eunsol <goldsolling@gmail.com>
2023-03-13 23:40:15 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,18 +1,51 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 5000
+
+const bodyParser = require('body-parser')
+const { User } = require("./models/User");
+
+const config = require('./config/key');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.json());
+
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://eunsolpark:dkffkqb77@cluster0.h6zvnwf.mongodb.net/?retryWrites=true&w=majority',{useNewUrlParser : true, useUnifiedTopology: 
-true
+mongoose.connect(config.mongoURL,
+{useNewUrlParser : true, useUnifiedTopology: true
 }).then(() => console.log('MongoDB Connected...'))
  .catch(err => console.log(err))
 
 
+
 app.get('/', (req, res) => {
-  res.send('Hello World!!')
+  res.send('다 울었니,, 해야할일을하자!!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.post('/register',async (req, res) => {
+  //회원 가입할 때 필요한 정보들을 Client에서 가져오면
+  //그것들은 DB에 넣어줌
+
+
+    //body parser통해 body에 담긴 정보를 가져온다
+    const user = new User(req.body)
+
+    const result = await user.save().then(()=>{
+      res.status(200).json({
+        succes : true
+      })
+     }).catch((err)=>{
+      res.json({success:false,err})
+     }) 
+
+
+    
+    })
+
+
+
+
+app.listen(port, () => 
+  console.log(`Example app listening on port ${port}!`))
